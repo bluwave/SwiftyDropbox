bd6dc62f5c4d5769939971d67dcac07053d96dd4
Garrett Richards <garrettrichards@gmail.com>
2015-12-29 21:08:01 -0800

    added tvOS target

---
+++ SwiftyDropboxTV/SwiftyDropboxTV.h
@@ -0,0 +1,19 @@
+//
+//  SwiftyDropboxTV.h
+//  SwiftyDropboxTV
+//
+//  Created by Garrett Richards on 12/29/15.
+//  Copyright © 2015 Adam Kirk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftyDropboxTV.
+FOUNDATION_EXPORT double SwiftyDropboxTVVersionNumber;
+
+//! Project version string for SwiftyDropboxTV.
+FOUNDATION_EXPORT const unsigned char SwiftyDropboxTVVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyDropboxTV/PublicHeader.h>
+
+
